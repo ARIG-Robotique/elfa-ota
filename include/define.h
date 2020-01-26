fbf2ff68c604dab071562397529db62524e39bd0
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-01-26 21:06:12 +0100

    Pilotage des IO en HTTP

--- include/define.h
+++ include/define.h
@@ -4,4 +4,10 @@
 #define WIFI_AP_PASSWORD "password"
 #endif
 
-#define GPIO_WIFI_RESET A0
+// Inputs
+#define GPIO_WIFI_RESET     A0
+
+// Outputs
+#define GPIO_HEARTBEAT      D0
+#define GPIO_RESET_STM32    D1
+#define GPIO_BOOT0_STM32    D3
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
 #include "define.h"
 
-#include <FS.h>                   // This needs to be first, or it all crashes and burns...
+#include <FS.h>  
+#include <ArduinoJson.h>                
 
-#include <ESP8266WiFi.h>          // https://github.com/esp8266/Arduino
+#include <ESP8266WiFi.h>          
 
 #include <DNSServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>      
 
 #include <RemoteDebug.h>
 
 #include <ElegantOTA.h>
 
+// Prototype des fonctions
+
+// Business
+void heartBeat();
+void wifiReset();
+
+// HTTP
+void httpError();
+void httpRoot();
+void httpGetIO();
+void httpSetIO();
+void httpGetServos();
+void httpSetServos();
+
+// Déclaration des objets
 ESP8266WebServer server(80);
 RemoteDebug Debug;
-int lastValue;
+WiFiManager wifiManager;
 
+// Variables locales
+int heartTimePrec, heartTime;
+boolean heart;
+
+// Initialisation de la puce
 void setup() {
     // Configuration des IOs //
     // --------------------- //
@@ -25,10 +46,12 @@ void setup() {
     pinMode(GPIO_WIFI_RESET, INPUT_PULLUP);
 
     // Outputs
+    pinMode(GPIO_HEARTBEAT, OUTPUT);
+    pinMode(GPIO_RESET_STM32, OUTPUT);
+    pinMode(GPIO_BOOT0_STM32, OUTPUT);
 
-
-    // Peripheral configuration //
-    // ------------------------ //
+    // Configuration des périphériques //
+    // ------------------------------- //
 
     // Serial
     Serial.begin(115200);
@@ -38,61 +61,139 @@ void setup() {
 
     // WiFi
 
-    // Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-    
-    // Reset saved settings
-    if (digitalRead(GPIO_WIFI_RESET)) {
-        wifiManager.resetSettings();
-        Serial.println(" * RESET WIFI Settings");
-        delay(5000);
-
-        ESP.restart();
-    }
-
     // Fetches ssid and pass from eeprom and tries to connect
     // if it does not connect it starts an access point with the specified name
     // and goes into a blocking loop awaiting configuration
     wifiManager.autoConnect("ElfaOtaAP", WIFI_AP_PASSWORD);
 
     // If you get here you have connected to the WiFi
-    String hostname = WiFi.hostname();
-    IPAddress ip = WiFi.localIP();
-    Serial.println(" * Connected to :");
-    Serial.println("   -> SSID        : " + WiFi.SSID());
-    Serial.println("   -> Hostname    : " + hostname);
-    Serial.println("   -> IP          : " + ip.toString());
-
-    MDNS.begin(hostname);
+    MDNS.begin(WiFi.hostname());
     MDNS.addService("telnet", "tcp", 23);
 
     // Initialize RemoteDebug
-    Debug.begin(hostname);
+    Debug.begin(WiFi.hostname());
     Debug.setResetCmdEnabled(true); // Enable the reset command
 	Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
 	Debug.showColors(true); // Colors
 
-    // OTA Self
+    // OTA Itself !!
     ElegantOTA.begin(&server); 
 
     // Routing
-    server.on("/", []() {
-        File f = SPIFFS.open("/index.html", "r");
-        server.send(200, "text/html", f.readString());
-    });
-  
+    server.on("/", httpRoot);
+    server.on("/io", HTTP_GET, httpGetIO);
+    server.on("/io", HTTP_POST, httpSetIO);
+    server.on("/servos", HTTP_GET, httpGetServos);
+    server.on("/servos", HTTP_POST, httpSetServos);
+
     // Start WebServer
     server.begin();
-    lastValue = millis();
+    
+    // Etat par défaut //
+    // --------------- //
+
+    digitalWrite(GPIO_RESET_STM32, HIGH); // Enable STM32
+
+    heartTime = heartTimePrec = millis();
+	heart = false;
 }
 
 void loop() {
+    heartBeat();
+    wifiReset();
     server.handleClient();
     Debug.handle();
 
-    if (millis() - lastValue >= 1000) {
-        lastValue = millis();
-        debugI("Coucou");
+    if (Serial.available()) {
+        String inputJson = Serial.readString();
+        if (inputJson == "GET_IP") {
+            Serial.println(WiFi.localIP().toString());
+        }        
     }
+}
+
+// ------------------------------------------------------- //
+// -------------------- BUSINESS METHODS ----------------- //
+// ------------------------------------------------------- //
+
+void heartBeat() {
+	heartTime = millis();
+	if (heartTime - heartTimePrec > 1000) {
+		heartTimePrec = heartTime;
+		digitalWrite(LED_BUILTIN, (heart) ? HIGH : LOW);
+		heart = !heart;
+	}
+}
+
+void wifiReset() {
+    // Reset EEPROM saved settings
+    if (digitalRead(GPIO_WIFI_RESET)) {
+        wifiManager.resetSettings();
+        debugI(" * RESET WIFI Settings");
+
+        ESP.restart();
+        delay(10000); // Not continue
+    }
+}
+
+// ------------------------------------------------------- //
+// ---------------------- HTTP METHODS ------------------- //
+// ------------------------------------------------------- //
+
+void httpError() {
+    debugE("Affichage de la page d'erreur");
+    File f = SPIFFS.open("/404.html", "r");
+    server.send(404, "text/html", f.readString());
+    f.close();
+}
+
+void httpRoot() {
+    debugI("Page racine");
+    File f = SPIFFS.open("/index.html", "r");
+    server.send(200, "text/html", f.readString());
+    f.close();
+}
+
+void httpGetIO() {
+    debugI("Liste des IOs");
+    server.send(204, "application/json", "{}");
+}
+
+void httpSetIO() {
+    StaticJsonDocument<256> query;
+    StaticJsonDocument<256> response;
+    String jsonResponse;
+    int httpCode;
+
+    debugI("Changement d'une IO");
+
+    DeserializationError err = deserializeJson(query, server.arg("plain"));
+    if (err) {
+        debugE("DeserializeJson() failed with code %s", err.c_str());
+        response["error"] = err.c_str();
+        httpCode = 500;
+
+    } else {
+        String io = query["ioName"];
+        boolean value = query["ioValue"];
+
+        if (io == "RESET_STM32") {
+            digitalWrite(GPIO_RESET_STM32, value);
+            httpCode = 204;
+        } else {
+            response["error"] = "IO inconnu";
+            httpCode = 404;
+        }
+    }
+
+    serializeJson(response, jsonResponse);
+    server.send(httpCode, "application/json", jsonResponse);
+}
+
+void httpGetServos() {
+
+}
+
+void httpSetServos() {
 
 }
